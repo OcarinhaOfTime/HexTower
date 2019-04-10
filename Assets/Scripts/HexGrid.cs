0c809f286954580481d71aa7966be38c74a444eb
Vagner Seibert <vagnerseibert@gmail.com>
2019-04-10 09:54:21 -0300

    Implemented cube and offset coordinates, with conversions. Implemented neighbor's search. Added TextMeshPRO.

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +7,84 @@ public class HexGrid : MonoBehaviour {
     public int width = 16;
     public int height = 16;
     public float hex_size = 1;
-    public Transform hexPrefab;
+    public Hexagon hexPrefab;
+    public bool useCubeCoords;
+
+    private Dictionary<Vector3Int, Hexagon> hexagons_dict = new Dictionary<Vector3Int, Hexagon>();
+
+    public Color normalColor = Color.white;
+    public Color selectedColor = Color.white;
+    public Color neighborColor = Color.white;
+
+    private Vector3Int[] cube_directions = {
+        new Vector3Int(+1, -1, 0), new Vector3Int(+1, 0, -1), new Vector3Int(0, +1, -1),
+        new Vector3Int(-1, +1, 0),new  Vector3Int(-1, 0, +1), new Vector3Int(0, -1, +1)};
 
     private void Start() {
+        Action<Hexagon, Vector2Int> onCreate;
+        if (useCubeCoords)
+            onCreate = (he, c) => he.SetCoords(oddr_to_cube(c));
+        else
+            onCreate = (he, c) => he.SetCoords(c);
+
         var w = Mathf.Sqrt(3) * hex_size;
         var h = hex_size * 2;
-        hexPrefab.localScale = Vector3.one * hex_size * 2;
+        hexPrefab.transform.localScale = Vector3.one * hex_size * 2;
 
-        for (int i=0; i<width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = -width; i <= width; i++) {
+            for (int j = -height; j <= height; j++) {
                 var hex = Instantiate(hexPrefab);
-                float o = w * .5f * ((j + 1) % 2);
-                hex.position = new Vector3((i - width / 2) * w + o, (j - height / 2) * h * .75f, 0);
-                hex.name = "tile_"+i+":"+j;
-                hex.SetParent(transform);
+                float o = w * .5f * ((j + height) % 2);
+                hex.name = "tile_" + i + ":" + j;
+                hex.transform.SetParent(transform);
+                hex.transform.localPosition = new Vector3(i * w + o, j * h * .75f, 0);
                 hex.gameObject.SetActive(true);
+
+                Vector2Int coord = new Vector2Int(i, -j);
+                var ccoord = oddr_to_cube(coord);
+                onCreate(hex, coord);
+                hexagons_dict.Add(ccoord, hex);
             }
         }
+    }
+
+    public static Vector2Int cube_to_oddr(Vector3Int coord) {
+        var x = coord.x + (coord.z - (coord.z & 1)) / 2;
+        var y = coord.z;
+
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector3Int oddr_to_cube(Vector2Int coord) {
+        var x = coord.x - (coord.y - (coord.y & 1)) / 2;
+        var z = coord.y;
+        var y = -x - z;
+
+        return new Vector3Int(x, y, z);
+    }
+
+    public List<Hexagon> Neighbors(Hexagon hex) {
+        List<Hexagon> neighbors = new List<Hexagon>();
+
+        foreach(var d in cube_directions) {
+            var nc = hex.coord + d;
+            if (hexagons_dict.ContainsKey(nc))
+                neighbors.Add(hexagons_dict[nc]);
+        }
+
+        return neighbors;
+    }
+
+    public void ResetColors() {
+        foreach (var h in hexagons_dict.Values)
+            h.SetColor(normalColor);
+    }
+
+    public void OnClick(Hexagon hex) {
+        ResetColors();
+
+        hex.SetColor(selectedColor);
+        foreach (var h in Neighbors(hex))
+            h.SetColor(neighborColor);
     }
 }
---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour {
+    private GameObject offsetCoords;
+    private GameObject cubeCoords;
+
+    private TMP_Text offset_x_txt;
+    private TMP_Text offset_y_txt;
+
+    private TMP_Text cube_x_txt;
+    private TMP_Text cube_y_txt;
+    private TMP_Text cube_z_txt;
+
+    public Vector3Int coord;
+
+    private SpriteRenderer sr;
+
+    private HexGrid hexGrid;
+
+    private void Awake() {
+        offsetCoords = transform.GetChild(0).GetChild(0).gameObject;
+        cubeCoords = transform.GetChild(0).GetChild(1).gameObject;
+
+        offset_x_txt = offsetCoords.transform.GetChild(0).GetComponent<TMP_Text>();
+        offset_y_txt = offsetCoords.transform.GetChild(1).GetComponent<TMP_Text>();
+
+        cube_x_txt = cubeCoords.transform.GetChild(0).GetComponent<TMP_Text>();
+        cube_y_txt = cubeCoords.transform.GetChild(1).GetComponent<TMP_Text>();
+        cube_z_txt = cubeCoords.transform.GetChild(2).GetComponent<TMP_Text>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+        hexGrid = GetComponentInParent<HexGrid>();
+    }
+
+    public void SetCoordsActive(bool isCubeCoords) {
+        offsetCoords.SetActive(!isCubeCoords);
+        cubeCoords.SetActive(isCubeCoords);
+    }
+
+    public void SetCoords(Vector2Int coord) {
+        this.coord = HexGrid.oddr_to_cube(coord);
+        offset_x_txt.text = "" + coord.x;
+        offset_y_txt.text = "" + coord.y;
+
+        SetCoordsActive(false);
+    }
+
+    public void SetCoords(Vector3Int coord) {
+        this.coord = coord;
+        cube_x_txt.text = "" + coord.x;
+        cube_y_txt.text = "" + coord.y;
+        cube_z_txt.text = "" + coord.z;
+
+        SetCoordsActive(true);
+    }
+
+    public void SetColor(Color color) {
+        sr.color = color;
+    }
+
+    private void OnMouseDown() {
+        print("you've clicked on " + coord);
+        hexGrid.OnClick(this);
+    }
+}
